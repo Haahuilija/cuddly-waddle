220d57f16a3462cd2bdde941a9a89fcbcc88f7d0
SaleZorro <saul.koskinen@gmail.com>
2023-04-15 15:17:35 +0300

    oops

--- tua-website/env.ts
+++
@@ -1,4 +0,0 @@
-export const EMAIL_TO = 'saul.koskinen@outlook.com';
-export const EMAIL_FROM = 'saul.koskinen@gmail.com';
-export const NEXT_PUBLIC_RECAPTCHA_SITE_KEY='6Ldm234lAAAAAFXIq05bBZbIXMpZO7CObP_kZlHO';
-export const NEXT_PUBLIC_RECAPTCHA_PROJECT_ID='tua-website-1681296175377';
