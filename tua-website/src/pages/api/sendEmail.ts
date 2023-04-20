a886d3ee513b300ddfc68e64e5ff83436978e18a
Saul Koskinen <saul.koskinen@gmail.com>
2023-04-20 14:54:20 +0300

    sendEmail is working with the API key and email addresses stored in Secret Manager.

--- tua-website/src/pages/api/sendEmail.ts
+++ tua-website/src/pages/api/sendEmail.ts
@@ -1,20 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import sgMail from '@sendgrid/mail';
+import sgMail, { MailDataRequired } from '@sendgrid/mail'; // Add MailDataRequired import
 import { getSecretValues } from './secrets';
 
 export default async function sendEmail(req: NextApiRequest, res: NextApiResponse) {
-  const { name, email, message, schedule, other} = req.body;
+  const { name, email, message, schedule, other } = req.body;
 
   // Get the email address values
   const { EMAIL_FROM, EMAIL_TO, SENDGRID_API_KEY } = await getSecretValues();
 
   // Create the email message
-  const msg = {
-    to: await EMAIL_TO,
-    from: await EMAIL_FROM || 'default@example.com',
+  const msg: MailDataRequired = { // Use MailDataRequired instead of EmailData
+    to: EMAIL_TO || 'default@example.com',
+    from: EMAIL_FROM || 'default@example.com',
     subject: `New message from ${name} (${email})`,
     text: message,
     html: `<p>${message}</p>`,
+    content: [] // Add empty content array
   };
 
   // Add schedule and other if available
@@ -29,7 +30,7 @@ export default async function sendEmail(req: NextApiRequest, res: NextApiRespons
 
   console.log('Sending email...');
   try {
-    sgMail.setApiKey(await SENDGRID_API_KEY || '');
+    sgMail.setApiKey(SENDGRID_API_KEY || 'default');
     await sgMail.send(msg);
     console.log('Email sent successfully');
     res.status(200).send('Email sent successfully');
